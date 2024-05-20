127d260001b8b7fdf5f6deb4dc79a26260081d6e
Franziska Furtado <franziskafurtado@gmail.com>
2024-05-20 17:53:40 -0300

    sample de notificação
    pra mostrar os erros

--- Frontend/hectoliter-app/src/app/pages/wait/wait.page.ts
+++ Frontend/hectoliter-app/src/app/pages/wait/wait.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-wait',
@@ -8,13 +9,23 @@ import { Router } from '@angular/router';
 })
 export class WaitPage implements OnInit {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private toastController: ToastController) {
     setTimeout(() => {
       this.router.navigate(['/end']); // Mudar para a página desejada após o timeout
     }, 6000);
   }
+  async presentToast(position: 'top') {
+    const toast = await this.toastController.create({
+      message: 'Erro vai aqui',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
 
   ngOnInit() {
+
   }
 
 }
