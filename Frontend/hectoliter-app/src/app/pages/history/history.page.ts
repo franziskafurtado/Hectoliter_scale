d55fd2634db650f26fde480baaa22f12e415964f
joaopedrogaleracfy <joao.matta@cloudfy.net.br>
2024-05-22 16:52:55 -0300

    app pronto

--- Frontend/hectoliter-app/src/app/pages/history/history.page.ts
+++ Frontend/hectoliter-app/src/app/pages/history/history.page.ts
@@ -11,7 +11,18 @@ export class HistoryPage implements OnInit {
 
   onPage: boolean = false;
 
+  historyItens: any[] = [];
+  dataIni: any;
+  dataFim: any;
+  loading: boolean = false;
+
   constructor(private router: Router, private backend: BackendService) { }
+
+  ionViewWillEnter(){
+    this.dataIni = new Date().toISOString();
+    this.dataFim = new Date().toISOString();
+    this.historyItens = [];
+  }
 
   ionViewDidEnter(){
     this.onPage = true;
@@ -31,7 +42,74 @@ export class HistoryPage implements OnInit {
     this.router.navigate(['/main']);
   }
 
-  GoToEnd() {
-    this.router.navigate(['/end', {process: 5}]);
+  GoToProcess(id: any) {
+    this.router.navigate(['/end', {process: id}]);
+  }
+
+  async onSearch(){
+    let _dataIni = this.formatData(this.dataIni);
+    let _dataFim = this.formatData(this.dataFim);
+
+    let _url = '/processes';
+
+    if(_dataIni && !_dataFim){
+      _url = _url + '?start=' + _dataIni;
+    }
+    else if(!_dataIni && _dataFim){
+      _url = _url + '?end=' + _dataFim;
+    }
+    else if(_dataIni && _dataFim){
+      _url = _url + '?start=' + _dataIni + '&end=' + _dataFim;
+    }
+
+    try{
+      this.loading = true;
+      let _processes = await this.backend.doGet(_url);
+      if(!_processes || !Array.isArray(_processes)){
+        throw "Invalid response from server. Please contact suport"
+      }
+
+      this.historyItens = _processes.map((element: any) => {
+        return {
+          id: element.id,
+          status: element.status,
+          timestamp: new Date(element.timestamp).toLocaleString()
+        }
+      });
+
+    }
+    catch(err: any){
+      this.backend.presentToast(err.error??err);
+    }
+
+    this.loading = false;
+  }
+
+  formatData(iso: any){
+    let _data: any = iso ? new Date(iso) : null;
+
+    if(_data){
+      let _year: any = _data.getFullYear().toString();
+      let _month: any = _data.getMonth() + 1;
+      let _day: any = _data.getDate();
+
+      if(_month < 10){
+        _month = '0' + _month.toString();
+      }
+      else{
+        _month = _month.toString();
+      }
+
+      if(_day < 10){
+        _day = '0' + _day.toString();
+      }
+      else{
+        _day = _day.toString();
+      }
+
+      _data = _year + '-' + _month + '-' + _day;
+    }
+
+    return _data;
   }
 }
--- Frontend/hectoliter-app/src/app/pages/login/login.page.ts
+++ Frontend/hectoliter-app/src/app/pages/login/login.page.ts
@@ -11,24 +11,29 @@ export class LoginPage implements OnInit {
 
   user: any;
   pass: any;
+  loading: boolean = false;
 
   constructor(private router: Router, private backend: BackendService) { }
 
   ngOnInit() {
   }
 
-  doLogin(){
+  async doLogin(){
     try{
       if(!this.user || !this.pass){
         this.backend.presentToast("Username and password must be provided");
         return;
       }
 
-      this.backend.login(this.user,this.pass);
+      this.loading = true;
+
+      await this.backend.login(this.user,this.pass);
     }
     catch(err: any){
       this.backend.presentToast(err.error??err);
     }
+
+    this.loading = false;
   }
 
 }
