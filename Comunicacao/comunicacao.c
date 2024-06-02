8c954eb3d5353928315b827b999da99eec795484
Franziska Furtado <franziskafurtado@gmail.com>
2024-06-01 23:27:31 -0300

    Update comunicacao.c

--- Comunicacao/comunicacao.c
+++ Comunicacao/comunicacao.c
@@ -13,6 +13,7 @@ typedef struct
 int status = 0;
 int iniciar = 0;
 Erros lista_erros = {{10, 20, 30, 40, 50, 60, 70, 80}};
+// dados_finais
 
 // Definição das filas para comunicação entre as threads
 QueueHandle_t fila_status;
@@ -33,12 +34,34 @@ void task_comunicacao(void *pvParameters)
     }
 }
 
+void task_processo(void *pvParameters)
+{
+    while (1)
+    {
+
+        if (status == 3)
+        {
+            fim_processo();
+            break;
+        }
+    }
+}
+
+void fim_processo()
+{
+    printf("Fim do processo\n");
+
+    // Envia dados
+    // xQueueSend(fila_dados_finais, &dados_finais, portMAX_DELAY);
+}
+
 void app_main()
 {
     // Inicializa filas
     fila_status = xQueueCreate(1, sizeof(int));
     fila_iniciar = xQueueCreate(1, sizeof(int));
     fila_erros = xQueueCreate(1, sizeof(Erros));
+    // fila_dados_finais = xQueueCreate(1, sizeof(int));
 
     // Cria threads
     xTaskCreate(task_comunicacao, "Comunicacao", 4096, NULL, 5, NULL);
