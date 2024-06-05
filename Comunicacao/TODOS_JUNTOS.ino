60eabf60af574f3f6f154cda9a42ac4a8cadd041
Guilherme Saito <47851647+GuilhermeSaito@users.noreply.github.com>
2024-06-05 19:06:30 -0300

    Added alguns erros que podem acontecer

--- Comunicacao/TODOS_JUNTOS.ino
+++ Comunicacao/TODOS_JUNTOS.ino
@@ -11,7 +11,7 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include <ArduinoJson.h>
-#include <ESPping.h>
+#include <ESP32Ping.h>
 
 // --------------------------------------------- VARIAVEIS COMUNICACAO COM BANCO
 int status = 0;
@@ -658,8 +658,7 @@ void task_comunicacao(void *pvParameters)
           WiFiStatus = 0; 
           delay(50);
         }
-        const IPAddress ping_ip(8, 8, 8, 8);
-        while(!Ping.ping(ping_ip, 2)){
+        while(!Ping.ping("8.8.8.8", 2)){
           WiFiStatus = 0; 
           delay(50);
         }
